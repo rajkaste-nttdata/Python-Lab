f10c4074709926b6d0157b744badc6e874fac64b
Kaste <228964@NTTDATA.COM>
2022-06-30 17:08:33 +0530

    initial commit

--- q1-list-comprehension.py
+++ q1-list-comprehension.py
@@ -1,5 +1,5 @@
 list_of_lists = [[1, 2, 3, 4], ['a', 'b', 'c'], [5, 6, 'd']]
 
-# list comprehension
+# list comprehension expression
 flat_list = [item for sublist in list_of_lists for item in sublist]
 print(flat_list)
